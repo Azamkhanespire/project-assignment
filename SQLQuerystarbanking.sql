9874c8c13386a8f726d61b30ba71feebe87bb871
Azamkhanespire <113002941+Azamkhanespire@users.noreply.github.com>
2022-09-08 17:11:10 +0530

    Add files via upload

--- SQLQuerystarbanking.sql
+++ SQLQuerystarbanking.sql
@@ -1,130 +1,54 @@
 CREATE TABLE Insurance_Scheme(
-    Category varchar (255),
-	Plans varchar (255) PRIMARY KEY,
-	Receipient varchar (255), 
-	Tenure varchar (255),
-	Benefits varchar (255),
-	Recover varchar(255),
-	Renewal varchar(255),
-) 
+    Plan_Id INT PRIMARY KEY,
+	Plan_Name VARCHAR (25),
+	Plan_Amount INT,
+	Receipient INT,
+	Tenure VARCHAR (40),
+	Renew_Plan INT,
 
-insert into Insurance_Scheme values ('Premium', 49999, 7, '1 year', 'Ambulence, Free Bed, Meal, Recover Benefits', 'Get 20% off','yearly')
-insert into Insurance_Scheme values ('Gold', 39999, 3, '1 year', 'Ambulence, Free Bed, Meal', 'Get 15% off','yearly')
-insert into Insurance_Scheme values ('Silver', 27999, 3, '1 year', 'Ambulence, Free Bed', 'Get 10% off', 'yearly')
-insert into Insurance_Scheme values ('Life cover', 28999, 18, '1 year', 'Ambulence, Free Bed', 'Get 5% off','yearly')
-insert into Insurance_Scheme values ('Critical illness', 21999, 30, '1 year', 'Ambulence, Free Bed', 'Get 5% off','yearly')
-insert into Insurance_Scheme values ('Accidenatal cover', 19999, 30, '1 year', 'Assuring all benefits', 'Decided on situation','yearly')
-insert into Insurance_Scheme values ('1 crore life cover', 23999, 63, '1 year', 'Assuring all benefits', 'immidiate','yearly')
-insert into Insurance_Scheme values ('life cover till 99', 60000, 6, '99 year', 'Assuring all benefits', 'Whole life', 'yearly')
+	
+)
 
-Select *from Insurance_Scheme
 
+INSERT INTO Insurance_Scheme VALUES (1280,'Premium', 6850, 7536,'Yearly' ,6165 )
+INSERT INTO Insurance_Scheme VALUES (1281,'Gold', 9800, 373,'yearly',8820)
+INSERT INTO Insurance_Scheme VALUES (1283,'Silver', 8200, 674,'Halfyearly',7380)
+INSERT INTO Insurance_Scheme VALUES (1282,'Life cover', 8400, 764,'yearly',7560)
+INSERT INTO Insurance_Scheme VALUES (1285,'Critical illness', 6400, 8943,'Quaterly',5760)
+INSERT INTO Insurance_Scheme VALUES (1284,'Accidenatal cover', 7600,764,'Biannualy',6840)
+INSERT INTO Insurance_Scheme VALUES (1286,'1 crore life cover', 9200, 639,'yearly',8280)
+INSERT INTO Insurance_Scheme VALUES (1287,'life cover till 99', 6200, 6373, '99 Year',5580)
 
+SELECT *FROM Insurance_Scheme
 
+CREATE TABLE Category(
+Category_Id INT PRIMARY KEY,
+Plan_Id INT,
+Insurance_Types VARCHAR (25)
 
+CONSTRAINT FK_PLANS FOREIGN KEY(Plan_Id)
+REFERENCES Insurance_Scheme(Plan_Id)
+)
+ SELECT *FROM Category
 
 
+INSERT INTO Category VALUES (1001,1280,'Full life policy')
+INSERT INTO Category VALUES (1002, 1281,'Senior citizen Policy')
+INSERT INTO Category VALUES (1003,1283,'Health Policy')
+INSERT INTO Category VALUES (1004,1282,'Family Policy')
+INSERT INTO Category VALUES (1005,1284,'Accidental Policy ')
+INSERT INTO Category VALUES (1008,1285,'Child Policy')
+INSERT INTO Category VALUES (1006,1286,' Policy')
+INSERT INTO Category VALUES (1007,1287,'Retirement Policy')
+ 
+ 
 
 
 
-Create View V_Insurance_Scheme as Select Category,Plans,Receipient,Tenure,Benefits,Recover,Renewal from Insurance_Scheme
 
 
-Select * from V_Insurance_Scheme
+CREATE VIEW  V_Insurance_Scheme AS  SELECT Category,Plans,Receipient,Tenure,Benefits,Recover,Renewal FROM Insurance_Scheme
 
 
-Insert into V_Insurance_Scheme values('Dimond',59999,3,'1 year','Gold+Health Insurancecard','30% off','yearly')
-
-
-Delete from V_Insurance_Scheme where Category='Accidental'
-
-
-Alter View V_Insurance_Scheme
-as
-Select  Category,Plans,Receipient,Tenure,Benefits,Recover,Renewal from Insurance_Scheme 
-Insert into V_Insurance_Scheme values('Pearl',59990,3,'1 year','Gold+Health Insurancecard','30% off','qauterly')
-
-
-Drop View V_Insurance_Scheme
-Create Procedure Proc_Insurance_Scheme_Insert
-
-    @VCategory varchar(255),
-	@VPlans varchar(255), 
-	@VReceipient varchar(255),
-	@VTenure varchar(255),
-	@VBenefits varchar(255),
-	@VRecover varchar(255),
-	@VRenewal varchar(255)
-    AS
-  Begin Transaction
-  Insert into Insurance_Scheme( Category,Plans,Receipient,Tenure,Benefits,Recover,Renewal)
-  values@VCategory,@VPlans,@VReceipient,@VTenure,@VBenefits,@VRecover,@VRenewal)
-Commit Transaction
-
-Exec Proc_Insurance_Scheme_Insert 'pearl','59999','3','1 year','Gold+Health Insurancecard','30% off','qauterly'
-
-Select * from Insurance_Scheme
-
-Alter Procedure Proc_Insurance_Scheme_Insert
-
-
-    @VCategory varchar(255),
-	@VPlans varchar(255), 
-	@VReceipient varchar(255),
-	@VTenure varchar(255),
-	@VBenefits varchar(255),
-	@VRecover varchar(255),
-	@VRenewal varchar(255)
-As
-
-
-
-Begin
-
-
-
- Begin Transaction
-
-
-
-   if ((len(@vCategory) > 0) And (len(@VPlans) > 0))
-
-
-
-   Begin
-
-
-
-     Insert into Insurance_Scheme(Category,Plans,Receipient,Tenure,Benefits,Recover,Renewal) values(@VCategory,@VPlans,@VReceipient,@VTenure,@VBenefits,@VRecover,@VRenewal)
-
-
-
-   End
-
-
-
-   Else
-
-
-
-   Begin
-
-
-
-     Print 'Cannot insert blank Branch Name and / Locate'
-
-
-
-   End
-
-
-
- Commit Transaction
-
-
-
-End
-
-
-
-Exec Proc_Insurance_Scheme_Insert 'Accidental',70000,3,'1 year','Gold+Health Insurancecard','30% off','qauterly'
+SELECT * FROM V_Insurance_Scheme
+ 
